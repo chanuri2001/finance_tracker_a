23e38d6f6bc92255430cee950921b645132214ea
Chanuri Nethma Amarasinghe <164017419+chanuri2001@users.noreply.github.com>
2025-03-02 16:53:48 +0530

    7

--- routes/categoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addCategory, getCategories } = require('../controllers/categoryController');
-const protect = require('../middleware/authMiddleware');
-
-router.post('/', protect, addCategory);
-router.get('/', protect, getCategories);
-
-module.exports = router;
